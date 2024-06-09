dd71b5a014eb46693cf7d4cd009a5f2bfd8287af
fvvelasco <fcvelasco1@up.edu.ph>
2024-06-10 07:05:45 +0800

    Attempt to fix some bugs

--- lib/pages/add_contact.dart
+++ lib/pages/add_contact.dart
@@ -7,138 +7,126 @@ class AddContactPage extends StatefulWidget {
   _AddContactPageState createState() => _AddContactPageState();
 }
 
-class _AddContactPageState extends State<AddContactPage>{
-    String? firstname;
-    String? lastname;
-    String? email;
-    String? phone;
-    final _formKey = GlobalKey<FormState>();
+class _AddContactPageState extends State<AddContactPage> {
+  String? firstname;
+  String? lastname;
+  String? email;
+  String? phone;
+  final _formKey = GlobalKey<FormState>();
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Add Contact",
-          style: TextStyle(
-            color: Colors.white
+        appBar: AppBar(
+          title: const Text(
+            "Add Contact",
+            style: TextStyle(color: Colors.white),
           ),
-        ), 
-      backgroundColor: Colors.blue,),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Form(
-          key:_formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start, 
-            children: [
-              firstnameField,
-              lastnameField,
-              emailField,
-              numberField,
-              addContactButton
-            ]
-          )
+          backgroundColor: Colors.blue,
         ),
-      )
-    );
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Form(
+              key: _formKey,
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    firstnameField,
+                    lastnameField,
+                    emailField,
+                    numberField,
+                    addContactButton
+                  ])),
+        ));
   }
 
   // Requires Firstname and Lastname field to have an input
   Widget get firstnameField => Padding(
-    padding: const EdgeInsets.all(8.0),
-    child:
-      TextFormField(
-        decoration: const InputDecoration(
-           border: OutlineInputBorder(),
-          label: Text("First Name"),
+        padding: const EdgeInsets.all(8.0),
+        child: TextFormField(
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(),
+            label: Text("First Name"),
+          ),
+          onSaved: (value) => setState(() => firstname = value),
+          validator: (value) {
+            if (value == null || value.isEmpty) {
+              return "Please enter a first name";
+            }
+            return null;
+          },
         ),
-        onSaved: (value) => setState(() => firstname = value),
-        validator: (value) {
-          if (value == null || value.isEmpty) {
-            return "Please enter a first name";
-          }
-          return null;
-        },
-      ),
-  );
+      );
 
   Widget get lastnameField => Padding(
-    padding: const EdgeInsets.all(8.0),
-    child:
-      TextFormField(
-        decoration: const InputDecoration(
-          border: OutlineInputBorder(),
-          label: Text("Last Name"),
+        padding: const EdgeInsets.all(8.0),
+        child: TextFormField(
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(),
+            label: Text("Last Name"),
+          ),
+          onSaved: (value) => setState(() => lastname = value),
+          validator: (value) {
+            if (value == null || value.isEmpty) {
+              return "Please enter a last name";
+            }
+            return null;
+          },
         ),
-        onSaved: (value) => setState(() => lastname = value),
-        validator: (value) {
-          if (value == null || value.isEmpty) {
-            return "Please enter a last name";
-          }
-          return null;
-        },
-      ),
-  );
-  
-  // 
+      );
+
+  //
   Widget get emailField => Padding(
-    padding: const EdgeInsets.all(8.0),
-    child: 
-      TextFormField(
+        padding: const EdgeInsets.all(8.0),
+        child: TextFormField(
           decoration: const InputDecoration(
             border: OutlineInputBorder(),
             label: Text("Email"),
           ),
           onSaved: (value) => setState(() => email = value),
           validator: (value) {
-          if (!RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$').hasMatch(value!) && value.isNotEmpty)
-          {
-            return 'Please submit a valid email address';
-          }
-          return null;
-        },
-      ),
-  );
+            if (!RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')
+                    .hasMatch(value!) &&
+                value.isNotEmpty) {
+              return 'Please submit a valid email address';
+            }
+            return null;
+          },
+        ),
+      );
 
   Widget get numberField => Padding(
-    padding: const EdgeInsets.all(8.0),
-    child: 
-      TextFormField(
+        padding: const EdgeInsets.all(8.0),
+        child: TextFormField(
           decoration: const InputDecoration(
             border: OutlineInputBorder(),
             label: Text("Phone Number"),
           ),
           onSaved: (value) => setState(() => phone = value),
           validator: (value) {
-          return null;
-        },
-      ),
-  );
-  
+            return null;
+          },
+        ),
+      );
+
   Widget get addContactButton => Padding(
-    padding: const EdgeInsets.all(8.0),
-    child:
-      ElevatedButton(
-        onPressed: () async {
-          if (_formKey.currentState!.validate()) {
-            _formKey.currentState!.save();
-            final newContact = Contact()
-              ..name.first = firstname!
-              ..name.last = lastname!
-              ..phones = [Phone(phone!)]
-              ..emails = [Email(email!)];
-            await newContact.insert();
+      padding: const EdgeInsets.all(8.0),
+      child: ElevatedButton(
+          onPressed: () async {
+            if (_formKey.currentState!.validate()) {
+              _formKey.currentState!.save();
+              final newContact = Contact()
+                ..name.first = firstname!
+                ..name.last = lastname!
+                ..phones = [Phone(phone!)]
+                ..emails = [Email(email!)];
+              await newContact.insert();
 
-            if (mounted) Navigator.pop(context);
-          }
-        },
-        child: const Text("Add")
-      )
-
-  );
+              if (context.mounted) Navigator.pop(context);
+            }
+          },
+          child: const Text("Add")));
 }
-  
 
 /*
 ,*/
--- lib/pages/display_contact.dart
+++ lib/pages/display_contact.dart
@@ -7,61 +7,58 @@ class ContactPage extends StatefulWidget {
 }
 
 class _ContactPageState extends State<ContactPage> {
-
   @override
-  Widget build(BuildContext context){
-
+  Widget build(BuildContext context) {
     final contact = ModalRoute.of(context)!.settings.arguments as Contact;
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          contact.displayName,
-          style: const TextStyle(
-            color: Colors.white
+        appBar: AppBar(
+          title: Text(
+            contact.displayName,
+            style: const TextStyle(color: Colors.white),
           ),
+          backgroundColor: Colors.blue,
         ),
-        backgroundColor: Colors.blue,),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text('First name: ${contact.name.first}'),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text('Last name: ${contact.name.last}'),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-                'Phone number: ${contact.phones.isNotEmpty ? contact.phones.first.number : '(none)'}'),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-                'Email address: ${contact.emails.isNotEmpty ? contact.emails.first.address : '(none)'}'),
-          ),
-          ElevatedButton(
-            onPressed: () async {
-              await contact.delete();
-              if (mounted) Navigator.pop(context);
-            }, 
-            child: const Text("Delete")
-          ),
-          ElevatedButton(                         // Delete Button
-            onPressed: () async {
-              if (context.mounted) {
-                Navigator.of(context).pushNamed(
-                  '/editcontact',
-                  arguments: contact,
-                );
-              }
-            },
-            child: const Text("Edit")
-          ),
-        ]),
-      ));
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('First name: ${contact.name.first}'),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('Last name: ${contact.name.last}'),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                  'Phone number: ${contact.phones.isNotEmpty ? contact.phones.first.number : '(none)'}'),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                  'Email address: ${contact.emails.isNotEmpty ? contact.emails.first.address : '(none)'}'),
+            ),
+            ElevatedButton(
+                onPressed: () async {
+                  await contact.delete();
+                  if (context.mounted) Navigator.pop(context);
+                },
+                child: const Text("Delete")),
+            ElevatedButton(
+                // Delete Button
+                onPressed: () async {
+                  if (context.mounted) {
+                    Navigator.of(context).pushNamed(
+                      '/editcontact',
+                      arguments: contact,
+                    );
+                  }
+                },
+                child: const Text("Edit")),
+          ]),
+        ));
   }
 }
